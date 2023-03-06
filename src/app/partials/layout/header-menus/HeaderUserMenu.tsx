0a24f6d0ec6315107055937ca7e766eee245a2e0
Van Dang <dangnv@stdio.asia>
2023-03-06 11:20:17 +0700

    feature/logo: change logo

--- src/app/partials/layout/header-menus/HeaderUserMenu.tsx
+++ src/app/partials/layout/header-menus/HeaderUserMenu.tsx
@@ -23,7 +23,7 @@ const HeaderUserMenu: FC = () => {
       <div className='menu-item px-3'>
         <div className='menu-content d-flex align-items-center px-3'>
           <div className='symbol symbol-50px me-5'>
-            <img alt='Logo' src={toAbsoluteUrl('/media/logo.png')} />
+            <img alt='Logo' src={toAbsoluteUrl('/media/logo.jpg')} />
           </div>
 
           <div className='d-flex flex-column'>
